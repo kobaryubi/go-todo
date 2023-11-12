9290cf3f5b8866f5e8817881c3339fb801ead0c5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-11-12 14:32:14 +0900

    feat: generate private key

--- internal/pkg/jwt/jwt.go
+++ internal/pkg/jwt/jwt.go
@@ -1,6 +1,12 @@
 package jwt
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -21,4 +27,34 @@ func CreateToken(name string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func GenerateTokenKey() error {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	x509Encoded, err := x509.MarshalECPrivateKey(privateKey)
+	if err != nil {
+		return err
+	}
+
+	pemEncoded := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: x509Encoded,
+	})
+
+	file, err := os.Create("es256_private_key.pem")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(pemEncoded)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
